0dc762b35fbdbd9e4dbe6b3ba2660a061281b76b
LeidyAldana <LeidyMarcelaAldana@gmail.com>
2018-11-04 15:09:12 -0500

    Configuración del proxy

---
+++ proxy.sh
@@ -0,0 +1,7 @@
+#Se añade la configuracion del proxy para la universidad
+
+PROXY_DIR=http://10.20.4.15:3128
+
+export {HTTP,HTTPS,FTP,ALL,SOCKS,RSYNC}_PROXY=$PROXY_DIR
+export {http,https,ftp,all,socks,rsync}_proxy=$PROXY_DIR
+#export {NO_PROXY,no_proxy}="localhost,127.0.0.1,localaddress,.localdomain.com,10.0$
