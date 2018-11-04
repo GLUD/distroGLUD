e8c79c19602a6346d5cc61884e2bfc7610d8c762
LeidyAldana <LeidyMarcelaAldana@gmail.com>
2018-11-04 15:15:02 -0500

    Archivo principal

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Configuración del proxy 
+#./proxy.sh
+
+# Actualización del sistema
+sudo apt update
+sudo apt upgrade
+
+# Cambio de la imagen del Grub
+./grub.sh
+
+# Cambio de la imagen de inicio, que en Linux mint es lm
+./CambiarInicio.sh
+
+# Instalación de zsh para personalizar la terminal
+./guia_zsh.sh
