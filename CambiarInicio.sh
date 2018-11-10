1e3eb9dc2dc8497436cf0d8a583c0f5b0a26e6d4
nicolas240 <nicolasmadero24@gmail.com>
2018-11-10 00:24:33 -0500

    Se añade un wget, ya que funcionaba con variable local

--- CambiarInicio.sh
+++ CambiarInicio.sh
@@ -8,7 +8,8 @@ echo "!SE BORRARA EL LOGO QUE TENIA PARA EL INICIO"
 sudo -s
 cd /usr/share/plymouth/themes/mint-logo/
 rm -R mint-logo.png
-cp "${imagen}" ./mint-logo.png
+wget -O=mint-logo.png "${imagen}"
+	#wget con el parametro de output para cambiar el nombre
 chmod 644 mint-logo.png
 
 cd /
