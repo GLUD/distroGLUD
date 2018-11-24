1994dfa7760038f33664adbe0fa6e7f5b01a69d2
nicolas240 <nicolasmadero24@gmail.com>
2018-11-23 23:36:52 -0500

    *

--- grub.sh
+++ grub.sh
@@ -5,8 +5,7 @@
 
 echo "Url, imagen jpg o png (640x480)"
 #read imagen
-imagen="https://mfiles.alphacoders.com/736/736874.jpg"
-#https://raw.githubusercontent.com/grupo-glud/Logo-GLUD/master/kokopelli-fondo-negro.png"
+imagen="https://raw.githubusercontent.com/grupo-glud/Logo-GLUD/master/kokopelli-fondo-negro.png"
 
 cd /boot/grub
 
