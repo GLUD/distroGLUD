01efc40f204aedf0f7a3ffe3f80eab17999fb3c5
nicolas240 <nicolasmadero24@gmail.com>
2018-11-23 22:55:36 -0500

    Se arreglo script con error de sintaxus de wget

--- grub.sh
+++ grub.sh
@@ -5,7 +5,8 @@
 
 echo "Url, imagen jpg o png (640x480)"
 #read imagen
-imagen="https://raw.githubusercontent.com/grupo-glud/Logo-GLUD/master/kokopelli-fondo-negro.png"
+imagen="https://mfiles.alphacoders.com/736/736874.jpg"
+#https://raw.githubusercontent.com/grupo-glud/Logo-GLUD/master/kokopelli-fondo-negro.png"
 
 cd /boot/grub
 
