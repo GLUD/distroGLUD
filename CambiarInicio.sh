9cda1d0b57116abdfd85fed9721ca1af5561041d
nicolas240 <nicolasmadero24@gmail.com>
2018-11-23 23:43:09 -0500

    Se corrigio error de sintaxis en la linea 12

--- CambiarInicio.sh
+++ CambiarInicio.sh
@@ -3,14 +3,14 @@
 echo "Ruta de la imagen  png (128x115)"
 #read imagen
 imagen="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/NewTux.svg/300px-NewTux.svg.png"
+
 echo "!SE BORRARA EL LOGO QUE TENIA PARA EL INICIO"
 
-sudo -s
 cd /usr/share/plymouth/themes/mint-logo/
-rm -R mint-logo.png
-wget -O=mint-logo.png "${imagen}"
+sudo rm -R mint-logo.png
+sudo wget -O mint-logo.png "${imagen}"
 	#wget con el parametro de output para cambiar el nombre
-chmod 644 mint-logo.png
+sudo chmod 644 mint-logo.png
 
 cd /
 
