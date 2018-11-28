14c864e7911fbd05bd50246f86dcba116c1416f3
Alejandro Morales <37442544+alejomora999@users.noreply.github.com>
2018-11-28 11:35:48 -0500

    Corrección ruta que añade la imagen

--- fondobloqueo.sh
+++ fondobloqueo.sh
@@ -6,7 +6,7 @@ echo "SE CAMBIA TANTO FONDO DE PANTALLA COMO DE BLOQUEO"
 wget https://raw.githubusercontent.com/GLUD/distroGLUD/master/wallpaper.png;
 #IMAGEN TEMPORAL, AUN NO ESTÁ SUBIDA LA DE FONDO
 mv wallpaper.png cambioparafondo.jpg;
-gsettings set org.cinnamon.desktop.background picture-uri 'file:////home/alejandro/cambioparafondo.jpg';
+gsettings set org.cinnamon.desktop.background picture-uri 'file:///'$HOME'/cambioparafondo.jpg';
 #CAMBIAR EL NOMBRE DE LA MAQUINA, LA MIA SE LLAMA alejandro
 echo "SE HAN CAMBIADO LOS FONDOS"
 
