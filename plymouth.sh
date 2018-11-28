68e2fc8ab115d1545e502a44797a361c2388e5c5
Diego Alejandro Rodriguez Ayala <diego.magno120@gmail.com>
2018-11-28 18:39:26 -0500

    Fix detalles plymouth.sh

--- plymouth.sh
+++ plymouth.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Script que instala el tema "GLUD Logo" como nuevo tema de plymouth por dedecto.
+# Script que instala el tema "GLUD Logo" como nuevo tema de plymouth por defecto.
 
 # INFO: https://forums.linuxmint.com/viewtopic.php?t=233197
 
 # Comando para remover el plymouth instalado con este script:
-# $ sudo update-alternatives --remove default.plymouth /usr/share/plymouth/themes/glud-logo/glud-logo.plymouth
+# $ sudo update-alternatives --remove default.plymouth /usr/share/plymouth/themes/glud-logo/glud-logo.plymouth && rm -rf /usr/share/plymouth/themes/glud-logo
 
-# TODO: Mejorar imagen glud-logo16.png
+# TODO: Mejorar "glud-logo/imagen glud-logo16.png"
 
 THEME="glud-logo"
 PLYMOUTH_DIR="/usr/share/plymouth/themes"
