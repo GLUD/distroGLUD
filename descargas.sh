41849280dfa32b53133f1c02d794abbad8de8b75
Diego Alejandro Rodriguez Ayala <diego.magno120@gmail.com>
2018-11-28 18:15:06 -0500

    Script de plymouth (imagen de inicio) TODO: Mejorar glud-logo/imagen glud-logo16.png

--- descargas.sh
+++ descargas.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 # Script que mueve "cada dia" todas las descargas del directorio 'Descargas' a una carpeta
 # nueva nombrada con la fecha de ejecución del sistema.
@@ -14,6 +14,4 @@ PATHS=$(ls -I "*Descargas_*_*_*" $FOLDER | awk -v dir=$FOLDER '{print "\"" dir "
 if [ -n "$PATHS" ]; then
     mkdir -p $DIR
     echo $PATHS | xargs mv -t $DIR
-else
-    echo "No paso nada"
 fi
---
+++ plymouth.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Script que instala el tema "GLUD Logo" como nuevo tema de plymouth por dedecto.
+
+# INFO: https://forums.linuxmint.com/viewtopic.php?t=233197
+
+# Comando para remover el plymouth instalado con este script:
+# $ sudo update-alternatives --remove default.plymouth /usr/share/plymouth/themes/glud-logo/glud-logo.plymouth
+
+# TODO: Mejorar imagen glud-logo16.png
+
+THEME="glud-logo"
+PLYMOUTH_DIR="/usr/share/plymouth/themes"
+PLYMOUTH_THEME="$PLYMOUTH_DIR/$THEME/$THEME.plymouth"
+
+if [ $(id -u) -eq 0 ]; then
+    if [ -e $THEME ]; then
+        cp -rf "./$THEME" $PLYMOUTH_DIR
+        update-alternatives --install "$PLYMOUTH_DIR/default.plymouth" default.plymouth $PLYMOUTH_THEME 200 > /dev/null
+        update-alternatives --set default.plymouth $PLYMOUTH_THEME > /dev/null
+        update-initramfs -u
+    else
+        echo "Error - No existe el tema \"$THEME\" en el directorio."
+    fi
+else
+    echo "Error - Ejecute como root."
+fi
